898745ff9148718d14023679477eb84f38dd2cb1
Ceren <109301665+cerenkuru@users.noreply.github.com>
2024-09-17 16:55:06 +0300

    Add files via upload

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Input({label, id, ...props}){
+
+    return (
+        <p className="control">
+            <label htmlFor={id}>{label}</label>
+            <input id={id} name={id} required {...props}/>
+        </p>
+
+    );
+
+
+}
